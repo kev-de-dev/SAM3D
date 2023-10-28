3d2b903d9013693786602aecaf7a37aa768342be
Kev-the-Dev <kevin.bossu@orange.fr>
2023-10-28 10:03:03 +0200

    Update

--- script.js
+++ script.js
@@ -15,6 +15,20 @@ elements.forEach((element, index) => {
   element.style.animationDelay = `${delay}ms`;
   delay += 500; // Augmentez le délai pour chaque élément
 });
+document.addEventListener('mousemove', parallax);
+function parallax(e){
+    const elements = document.querySelectorAll(".parallax");
+    
+    elements.forEach(function(element){
+        const speed = element.getAttribute('data-speed');
+        
+        const x = (window.innerWidth - e.pageX * speed) / 100;
+        const y = (window.innerHeight - e.pageY * speed) / 100;
+        
+        element.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
 
 
 
